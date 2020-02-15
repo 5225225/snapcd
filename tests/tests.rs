8feff5c81b4f5db2869ad818b1c7c4bf688d3a75
5225225 <5225225@mailbox.org>
2020-02-15 14:20:19 +0000

    base32 round trip test takes vec instead of array

--- tests/tests.rs
+++ tests/tests.rs
@@ -60,10 +60,10 @@ proptest::proptest! {
     }
 
     #[test]
-    fn round_trip_base32(bytes: [u8; 1]) {
+    fn round_trip_base32(bytes: Vec<u8>) {
         let b32 = snapcd::to_base32(&bytes);
         let restored = snapcd::from_base32(&b32, bytes.len() * 8).unwrap();
-        assert_eq!(restored.as_slice(), bytes);
+        assert_eq!(restored.as_slice(), &*bytes);
     }
 
     #[test]
