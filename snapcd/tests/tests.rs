c77efc75920a09088440ca2cfc75edd197c9dc60
5225225 <5225225@mailbox.org>
2021-03-12 20:48:16 +0000

    some work on tests

--- snapcd/tests/tests.rs
+++ snapcd/tests/tests.rs
@@ -157,3 +157,69 @@ fn file_round_trip_test() {
 
     assert_eq!(&result, &v);
 }
+
+#[test]
+fn init_test() {
+    use assert_cmd::Command;
+
+    let dir = assert_fs::TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("snapcd").unwrap();
+
+    let assert = cmd.arg("init").current_dir(dir.path()).assert();
+
+    assert.success();
+}
+
+#[test]
+fn commit_test() {
+    use assert_cmd::Command;
+    use assert_fs::fixture::{FileWriteStr, PathChild};
+
+    let dir = assert_fs::TempDir::new().unwrap();
+
+    let assert = Command::cargo_bin("snapcd")
+        .unwrap()
+        .arg("init")
+        .current_dir(dir.path())
+        .assert();
+
+    assert.success();
+
+    dir.child("a").write_str("a").unwrap();
+    dir.child("b").write_str("old").unwrap();
+
+    let assert = Command::cargo_bin("snapcd")
+        .unwrap()
+        .arg("commit")
+        .arg("-m")
+        .arg("0")
+        .current_dir(dir.path())
+        .assert();
+
+    assert.success();
+
+    std::fs::remove_file(dir.child("a").path()).unwrap();
+    dir.child("b").write_str("new").unwrap();
+    dir.child("c").write_str("c").unwrap();
+
+    let assert = Command::cargo_bin("snapcd")
+        .unwrap()
+        .arg("status")
+        .current_dir(dir.path())
+        .assert();
+
+    let expected_output = indoc::indoc!(
+        "
+        HEAD: main [bewgow4u]
+        added:
+          c
+        deleted:
+          a
+        modified:
+          b
+    "
+    );
+
+    assert.success().stdout(expected_output);
+}
