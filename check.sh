4464224cbff7c3072279c50be04207ccbab964b9
5225225 <5225225@mailbox.org>
2021-09-26 17:31:59 +0100

    Change check.sh to check docs as well

--- check.sh
+++ check.sh
@@ -1,11 +1,11 @@
 set -e 
 
-cargo clean -p snapcd
-
 cargo fmt --all -- --check
 
 cargo +stable clippy --all-targets --all-features -- -D warnings
 
 cargo +nightly clippy --all-targets --all-features -- -D warnings || true
+
+(cd libsnapcd; cargo +nightly rustdoc -- -Z unstable-options --check; )
 
 cargo test --all
